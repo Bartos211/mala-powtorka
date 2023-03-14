ef198b387d1f853e00e3e49b4c4c72ca1f68a6bc
Bartek <bszostak8@gmail.com>
2023-03-14 20:31:28 +0100

    mala powtorka

--- mala powtorka.py
+++ mala powtorka.py
@@ -5,11 +5,15 @@ dict= {
         "Warzywniak":["Marchew","Seler","Rukola"]
       }
 for klucz,wartosc in dict.items():
-    print("Ide do: {},kupuje tam rzeczy: {}".format(klucz, wartosc))
+    print("Ide do {},kupuje tam rzeczy {}".format(klucz, wartosc))
     
 stores= ['Chleb','Pączek','Bułki','Marchew','Seler','Rukola']
-print(len(stores))
-print('W sumie kupuje 6 produktow')
+
+counter = 0
+for store in stores:
+        counter +=1
+print(counter)
+print('W sumie kupuje 6 produktów')
 
 for i in range(0,100):
         if i & 5 == 0:
