0d89382ce01f0d03b7d8192f0e1c52a1e616f286
Bartek <bszostak8@gmail.com>
2023-03-14 08:08:03 +0100

    mala powtorka

--- mala powtorka.py
+++ mala powtorka.py
@@ -1,20 +1,11 @@
 name = "Lista zakupow"
 print(name)
 dict= {
-"Piekarnia":["Chleb","Pączek","Bułki"],
-"Warzywniak":["Marchew","Seler","Rukola"]
-}
-
-shop1 = "Piekarnia"
-shop2 = "Warzywniak"
-products1 = "Chleb","Pączek","Bułki"
-products2 = "Marchew","Seler","Rukola"
-shops = f"Idę do {shop1},kupuje tam {products1}"
-f"Idę do {shop2},kupuje tam{products2}"
-
-
-for klucz,wartość in dict.items():
-    print(shops)
+        "Piekarnia":["Chleb","Paczek","Bulki"],
+        "Warzywniak":["Marchew","Seler","Rukola"]
+      }
+for klucz,wartosc in dict.items():
+    print("Ide do: {},kupuje tam rzeczy: {}".format(klucz, wartosc))
     
 stores= ['Chleb','Pączek','Bułki','Marchew','Seler','Rukola']
 print(len(stores))
